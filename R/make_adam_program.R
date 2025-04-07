f111a9a314c83a84874b712219d653c33dd338ca
NOSJ (Nicolai Skov Johnsen) <nosj@novonordisk.com>
2025-04-07 12:52:06 +0200

    Clean up steps in make_adam_program()

--- R/make_adam_program.R
+++ R/make_adam_program.R
@@ -26,21 +26,21 @@ make_adam_program <- function(path_ui_data,
     lapply(parse_node_metadata) |>
     unlist(recursive = FALSE) |>
     update_ui_data(ui_data_1)
-  domain_initialization_metadata <- ui_data_2$init
-  trial_metadata <- ui_data_2$trial_metadata
   nodes <- convert_node_list_to_dt(ui_data_2$nodes)
 
   # Enrich predecessors in UI data with auto-generated metadata
   nodes_2 <- update_predecessors(nodes, path_domain_keys)
 
+  # Enrich UI data with predecessors that are not stated in the UI data and that
+  # are required for the derivations to be run
   nodes_4 <- add_implied_predecessors(nodes_2)
 
-  # extract_sdtm_core_variables_2(nodes_4)
-  nodes_5 <- create_domain_initialize_nodes(nodes_4, domain_initialization_metadata)
+  # Create an initialization action per domain that consumes predecessors
+  nodes_5 <- create_domain_initialize_nodes(nodes_4, ui_data_2$init)
+
   # This is done before external deps nodes are added (at the moment) because we
   # don't need to explicitly track the external deps in the topology
   edges <- make_edges(nodes_5)
-  nodes_6 <- enrich_with_external_dependencies(nodes_5, ui_data_2$init)
 
   unique_edges <- edges[, .SD[1], by = .(parent_node, node_id)][, .(parent_node, node_id)]
   graph <- igraph::graph_from_data_frame(unique_edges, directed = TRUE, vertices = nodes_5[, .(node_id, domain)])
@@ -48,6 +48,7 @@ make_adam_program <- function(path_ui_data,
   vertex_metadata <- nodes_5[, .(node_id, domain, type)]
   program_order <- group_nodes_optimal(topo_order_names, vertex_metadata, edges = edges)
 
+  nodes_6 <- enrich_with_external_dependencies(nodes_5, ui_data_2$init)
   program_order_2 <- add_program_init_nodes(program_order, nodes_6)
   program_order_3 <- add_nodes_to_load_external_data(program_order_2, nodes_6)
 
@@ -57,7 +58,7 @@ make_adam_program <- function(path_ui_data,
     nodes_6,
     domain_keys = yaml::read_yaml(path_domain_keys),
     std_library_path = path_std_lib,
-    trial_metadata,
+    ui_data_2$trial_metadata,
     data_connection
   )
 
