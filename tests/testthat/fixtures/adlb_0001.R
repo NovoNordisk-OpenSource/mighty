51b140675436f4e94bbbfff1e4b6e89f78b8f354
NOSJ (Nicolai Skov Johnsen) <nosj@novonordisk.com>
2025-04-15 14:32:12 +0200

    Correcting filter metadata for adlb in complex unit test

--- tests/testthat/fixtures/adlb_0001.R
+++ tests/testthat/fixtures/adlb_0001.R
@@ -332,7 +332,7 @@ aval_grp_02 <- function(.self) {
 # depend_cols:
 # outputs:
 #!-!
-ecopy_rows_01 <- function(.self) {
+copy_rows_01 <- function(.self) {
   .self <- rbind(.self, .self[1:10,])
   return(.self)
 }
