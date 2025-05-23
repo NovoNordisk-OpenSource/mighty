2717c3b061d05075a1ac2d0dba361c35c38d60ca
MHzT (Michael Hedegaard Thomsen) <mhzt@novonordisk.com>
2025-05-23 10:13:44 +0200

    Document and shorten line lengths to align with linting requirements

--- R/assert_valid_trial_config.R
+++ R/assert_valid_trial_config.R
@@ -1,3 +1,11 @@
+#' Assert trial_configuration is valid
+#'
+#' @description Validates that the trial configuration is a list and contains
+#' the required fields.
+#'
+#' @param trial_metadata A list containing trial metadata.
+#'
+#' @returns Nothing if trial configuration contains all needed fields
 assert_valid_trial_config <- function(trial_metadata) {
   # Check if trial_metadata is a list
   if (!is.list(trial_metadata)) {
@@ -8,13 +16,17 @@ assert_valid_trial_config <- function(trial_metadata) {
   required_fields <- c("trial_id", "project_id", "complete_id", "instance")
   missing_fields <- setdiff(required_fields, names(trial_metadata))
   if (length(missing_fields) > 0) {
-    stop(paste("Missing required fields in trial_metadata:", paste(missing_fields, collapse = ", ")))
+    stop(paste("Missing required fields in trial_metadata:",
+               paste(missing_fields, collapse = ", ")))
   }
 
   # Check if fields are non-empty
   for (field in required_fields) {
-    if (is.null(trial_metadata[[field]]) || !is.character(trial_metadata[[field]]) || trial_metadata[[field]] == "") {
-      stop(paste("Missing value for required fields in trial_metadata:", field, "cannot be NULL and must be a non-empty string."))
+    if (is.null(trial_metadata[[field]]) ||
+        !is.character(trial_metadata[[field]]) ||
+        trial_metadata[[field]] == "") {
+      stop(paste("Missing value for required fields in trial_metadata:", field,
+                 "cannot be NULL and must be a non-empty string."))
     }
   }
 }
