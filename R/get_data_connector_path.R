70852b9936fe891a13fa6a9d661a05f867c86ac9
MHzT (Michael Hedegaard Thomsen) <mhzt@novonordisk.com>
2025-05-23 11:45:15 +0200

    Add documentation

--- R/get_data_connector_path.R
+++ R/get_data_connector_path.R
@@ -1,3 +1,19 @@
+#' Get the path to the data connector for a given trial and data connection type
+#'
+#' @description This function generates the path to the data connector based on
+#' the \code{type} (\code{sdtm}, \code{adam} or \code{metadata}),
+#' \code{data_connection} type and the trial metadata.
+#'
+#' @param type A character string indicating the type of data connector. Valid
+#' values are \code{type} (\code{sdtm}, \code{adam} or \code{metadata}.
+#' @param trial_metadata A list containing trial metadata. It must contain the
+#' \code{project_id}, \code{trial_id}, \code{complete_id}, and \code{instance}
+#' @param data_connection A character string indicating the type of data
+#' connection. Valid values are \code{connector} (returning a path in line with
+#' current NN UNIX-SCE requirement), \code{custom_data} or \code{pharmaverse}
+#'
+#' @note
+#' This will become obsolete when connector config is used instead.
 get_data_connector_path <- function(type = c("sdtm", "adam", "metadata"),
                                     trial_metadata,
                                     data_connection = c("connector", "custom_data"),
