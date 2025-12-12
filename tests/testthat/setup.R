a62a758d26085f14662408f279c0e28b1198d5dc
MEWP (Matthew Phelps) <MEWP@novonordisk.com>
2025-12-12 11:13:34 +0100

    fix: Promote warnings in test suite to errors and reduce verbosity of test logs (#124)

---
+++ tests/testthat/setup.R
@@ -0,0 +1,12 @@
+# Test Setup
+# This file runs once before all tests
+
+# warn=2 converts warnings to errors
+withr::local_options(
+  .new = list(
+    warn = 2,
+    connector.verbosity_level = "quiet",
+    styler.quiet = TRUE
+  ),
+  .local_envir = testthat::teardown_env()
+)
